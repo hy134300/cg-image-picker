3f1fd7a1285509e8399ec3a4132a1b7e4a10803c
hy134300 <54663651+hy134300@users.noreply.github.com>
2024-04-16 15:18:02 +0800

    Add files via upload

--- image_chooser_preview.py
+++ image_chooser_preview.py
@@ -1,3 +1,8 @@
+import string
+import threading
+
+import folder_paths
+from custom_nodes.ComfyUI_Sound_NODE import huiben
 from server import PromptServer
 from nodes import PreviewImage
 from comfy.model_management import InterruptProcessingException
@@ -6,38 +11,46 @@ from .image_chooser_server import MessageHolder, Cancelled
 import torch
 import random
 
+path = ''
+lock = threading.Lock()
+
 class PreviewAndChoose(PreviewImage):
-    RETURN_TYPES = ("IMAGE","LATENT","MASK","STRING")
-    RETURN_NAMES = ("images","latents","masks","selected")
+    RETURN_TYPES = ("IMAGE", "LATENT", "MASK", "STRING")
+    RETURN_NAMES = ("images", "latents", "masks", "selected")
     FUNCTION = "func"
     CATEGORY = "image_chooser"
-    INPUT_IS_LIST=True
+    INPUT_IS_LIST = True
     OUTPUT_NODE = False
     last_ic = {}
+
     @classmethod
     def INPUT_TYPES(s):
         return {
             "required": {
-                "mode" : (["Always pause", "Repeat last selection", "Only pause if batch", "Progress first pick", "Pass through", "Take First n", "Take Last n"],{}),
-				"count": ("INT", { "default": 1, "min": 1, "max": 999, "step": 1 }),
+                "mode": (
+                    ["Always pause", "Repeat last selection", "Only pause if batch", "Progress first pick",
+                     "Pass through",
+                     "Take First n", "Take Last n"], {}),
+                "count": ("INT", {"default": 1, "min": 1, "max": 999, "step": 1}),
+
             },
-            "optional": {"images": ("IMAGE", ), "latents": ("LATENT", ), "masks": ("MASK", ) },
-            "hidden": {"prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO", "id":"UNIQUE_ID"},
+            "optional": {"images": ("IMAGE",), "latents": ("LATENT",), "masks": ("MASK",), "path": ("STRING", {"default": "ComfyUI"}),},
+            "hidden": {"prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO", "id": "UNIQUE_ID"},
         }
 
     @classmethod
     def IS_CHANGED(cls, mode, id, **kwargs):
-        if (mode[0]!="Repeat last selection" or not id[0] in cls.last_ic): cls.last_ic[id[0]] = random.random()
+        if (mode[0] != "Repeat last selection" or not id[0] in cls.last_ic): cls.last_ic[id[0]] = random.random()
         return cls.last_ic[id[0]]
 
     def func(self, id, **kwargs):
         # mode doesn't exist in subclass
-        self.count = int(kwargs.pop('count', [1,])[0])
-        mode = kwargs.pop('mode',["Always pause",])[0]
-        if mode=="Repeat last selection":
+        self.count = int(kwargs.pop('count', [1, ])[0])
+        mode = kwargs.pop('mode', ["Always pause", ])[0]
+        if mode == "Repeat last selection":
             print("Here despite 'Repeat last selection' - treat as 'Always pause'")
             mode = "Always pause"
-        if mode=="Always pause":
+        if mode == "Always pause":
             # pretend it was Repeat last so that the prompt matches if that is selected next time.
             # UGH
             kwargs['prompt'][0][id[0]]['inputs']['mode'] = "Repeat last selection"
@@ -48,87 +61,103 @@ class PreviewAndChoose(PreviewImage):
 
         # enable stashing. If images is None, we are operating in read-from-stash mode
         if 'images' in kwargs:
-            my_stash['images']  = kwargs['images']
+            my_stash['images'] = kwargs['images']
             my_stash['latents'] = kwargs.get('latents', None)
-            my_stash['masks']   = kwargs.get('masks', None)
+            my_stash['masks'] = kwargs.get('masks', None)
         else:
-            kwargs['images']  = my_stash.get('images', None)
+            kwargs['images'] = my_stash.get('images', None)
             kwargs['latents'] = my_stash.get('latents', None)
-            kwargs['masks']   = my_stash.get('masks', None)
-            
+            kwargs['masks'] = my_stash.get('masks', None)
+
         if (kwargs['images'] is None):
             return (None, None, None, "")
-        
+
         # convert list to batch
-        images_in         = torch.cat(kwargs.pop('images'))
-        latents_in        = kwargs.pop('latents', None)
-        masks_in          = torch.cat(kwargs.get('masks')) if kwargs.get('masks', None) is not None else None
+        images_in = torch.cat(kwargs.pop('images'))
+        latents_in = kwargs.pop('latents', None)
+        masks_in = torch.cat(kwargs.get('masks')) if kwargs.get('masks', None) is not None else None
         kwargs.pop('masks', None)
         latent_samples_in = torch.cat(list(l['samples'] for l in latents_in)) if latents_in is not None else None
-        self.batch        = images_in.shape[0]
-
+        self.batch = images_in.shape[0]
+        global path
+        if  kwargs.get('path'):
+            path = str(kwargs.pop('path', [1, ])[0])
+        else:
+            path = self.path
         # any other parameters shouldn't be lists any more...
         for x in kwargs: kwargs[x] = kwargs[x][0]
- 
         # call PreviewImage base
+        #ret =  huiben.sa(self, images=images_in,filename_prefix=path,prompt=None,extra_pnginfo=None,)
         ret = self.save_images(images=images_in, **kwargs)
-
+        self.path = path
+        kwargs['path'] = path
         # send the images to view
-        PromptServer.instance.send_sync("early-image-handler", {"id": id, "urls":ret['ui']['images']})
+        PromptServer.instance.send_sync("early-image-handler", {"id": id, "urls": ret['ui']['images']})
 
         # wait for selection
         try:
             is_block_condition = (mode == "Always pause" or mode == "Progress first pick" or self.batch > 1)
             is_blocking_mode = (mode not in ["Pass through", "Take First n", "Take Last n"])
-            selections = MessageHolder.waitForMessage(id, asList=True) if (is_blocking_mode and is_block_condition) else [0]
+            selections = MessageHolder.waitForMessage(id, asList=True) if (
+                    is_blocking_mode and is_block_condition) else [0]
         except Cancelled:
             raise InterruptProcessingException()
-            #return (None, None,)
-        
-        return self.batch_up_selections(images_in=images_in, latent_samples_in=latent_samples_in, masks_in=masks_in, selections=selections, mode=mode)
+            # return (None, None,)
+        return self.batch_up_selections(images_in=images_in, latent_samples_in=latent_samples_in, masks_in=masks_in,
+                                        selections=selections, mode=mode)
 
-    def tensor_bundle(self, tensor_in:torch.Tensor, picks):
+    def tensor_bundle(self, tensor_in: torch.Tensor, picks):
         if tensor_in is not None and len(picks):
             batch = tensor_in.shape[0]
-            return torch.cat(tuple([tensor_in[(x)%batch].unsqueeze_(0) for x in picks])).reshape([-1]+list(tensor_in.shape[1:]))
+            my_tuple = tuple([tensor_in[(x) % batch].unsqueeze_(0) for x in picks])
+            for i in range(len(my_tuple)):
+                self1 = {"output_dir": folder_paths.get_output_directory(), "type": "output", "prefix_append": "\"",
+                         "compress_level": 4}
+                a = self.path + str(picks[i]);
+                huiben.saveImageNew(self=self1, extra_pnginfo=None, filename_prefix=a,
+                                    images=my_tuple[i], prompt=None)
+            return torch.cat(my_tuple).reshape([-1] + list(tensor_in.shape[1:]))
         else:
             return None
-    
-    def latent_bundle(self, latent_samples_in:torch.Tensor, picks):
+
+    def latent_bundle(self, latent_samples_in: torch.Tensor, picks):
         if (latent_samples_in is not None and len(picks)):
-            return { "samples" : self.tensor_bundle(latent_samples_in, picks) }
+            return {"samples": self.tensor_bundle(latent_samples_in, picks)}
         else:
             return None
-    
-    def batch_up_selections(self, images_in:torch.Tensor, latent_samples_in:torch.Tensor, masks_in:torch.Tensor, selections, mode):
-        if (mode=="Pass through"):
+
+    def batch_up_selections(self, images_in: torch.Tensor, latent_samples_in: torch.Tensor, masks_in: torch.Tensor,
+                            selections, mode):
+        if (mode == "Pass through"):
             chosen = range(0, self.batch)
-        elif (mode=="Take First n"):
+        elif (mode == "Take First n"):
             end = self.count if self.batch >= self.count else self.batch
             chosen = range(0, end)
-        elif (mode=="Take Last n"):
+        elif (mode == "Take Last n"):
             start = self.batch - self.count if self.batch - self.count >= 0 else 0
             chosen = range(start, self.batch)
         else:
-            chosen = [x for x in selections if x>=0]
+            chosen = [x for x in selections if x >= 0]
+        return (self.tensor_bundle(images_in, chosen), self.latent_bundle(latent_samples_in, chosen),
+                self.tensor_bundle(masks_in, chosen), ",".join(str(x) for x in chosen),)
 
-        return (self.tensor_bundle(images_in, chosen), self.latent_bundle(latent_samples_in, chosen), self.tensor_bundle(masks_in, chosen), ",".join(str(x) for x in chosen), )
-    
+
 class PreviewAndChooseDouble(PreviewAndChoose):
-    RETURN_TYPES = ("LATENT","LATENT",)
-    RETURN_NAMES = ("positive","negative",)
+    RETURN_TYPES = ("LATENT", "LATENT",)
+    RETURN_NAMES = ("positive", "negative",)
+
     @classmethod
     def INPUT_TYPES(s):
         return {
             "required": {
-                "images": ("IMAGE", ), 
-                "latents": ("LATENT", ),
+                "images": ("IMAGE",),
+                "latents": ("LATENT",),
             },
-            "hidden": {"prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO", "id":"UNIQUE_ID"},
-        } 
+            "hidden": {"prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO", "id": "UNIQUE_ID"},
+        }
 
-    def batch_up_selections(self, images_in, latent_samples_in, masks_in, selections:list, mode):
+    def batch_up_selections(self, images_in, latent_samples_in, masks_in, selections: list, mode):
         divider = selections.index(-1)
         latents_out_good = self.latent_bundle(latent_samples_in, selections[:divider])
-        latents_out_bad = self.latent_bundle(latent_samples_in, selections[divider+1:])
+        latents_out_bad = self.latent_bundle(latent_samples_in, selections[divider + 1:])
         return (latents_out_good, latents_out_bad,)
